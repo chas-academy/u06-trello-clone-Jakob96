cc81e3eb252718e1314024a49d33d6cf24629501
Jakob96 <jakob96@outlook.com>
2021-03-01 16:30:07 +0100

    Added text for empty lists

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 $( function() {
     $( ".card-layout" ).tabs();
 
+    $( ".add-card" ).click(function(e) {
+        addCard(this);
+      });
+
     $( ".list" ).sortable({
+      update: function(event, ui) {
+        if ($("ul." + event.target.classList[1] + " li").length == 0) {
+            addEmptyListText(this);
+        }
+        else {
+            $("ul." + event.target.classList[1] + " li").remove(".empty-list");
+        }
+      },
         connectWith: ".list",
       }).disableSelection();
+
+      function addEmptyListText(element) {
+            const emptyListText = $("<li>").attr("class", "empty-list").html("Drag a card here");
+            $(element).append(emptyListText);
+      }
   } );
