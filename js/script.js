cb3837cd441f12fcc911d9393756ef149f70bdec
Jakob96 <jakob96@outlook.com>
2021-03-02 23:43:31 +0100

    Added buttons and events to add cards and sections

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $( function() {
     addCard("Test", "Todo");
     addCard("Test", "Todo");
 
-    $( ".card-layout, #tabs" ).tabs();
+   $( "#tabs" ).tabs();
 
     $("#card-info").dialog({
       autoOpen: false,
@@ -20,8 +20,20 @@ $( function() {
 
     $(".datepicker").datepicker();
 
-    $("button.edit, .card").on("click", function() {
+    $(".card-layout").on("click", "button.edit, .card", function() {
       $('#card-info').dialog('open').effect("bounce", {times: 2}, 250);
+    });
+
+    $("button.new-card").on("click", function() {
+      addCard("Write some text...", $(this).attr("data-list"));
+    });
+
+    $("button.add-col").on("click", function() {
+      const title = prompt("Enter title");
+      
+      if (title) {
+        addColumn(title);
+      }
     });
 
     $( ".list" ).sortable({
@@ -44,16 +56,19 @@ $( function() {
 
       function addColumn(title) {
           const section = $("<section>").attr("class", "card-layout");
-          const tab = $("<ul>").append("<li>").html(title);
+          const newCardBtn = $("<button>").attr({"class": "new-card float-right", "aria-label": "Add new card", "data-list": title.toLowerCase()}).html("+");
+          const tab = $("<ul>").append("<li>").html(title).append(newCardBtn);
           const emptyListText = $("<li>").attr("class", "empty-list").html("Drag a card here");
           const cardList = $("<ul>").attr("class", "list " + title.toLowerCase()).append(emptyListText);
       
           const col = section.append(tab).append(cardList);
           $("section.flex-grid").append(col);
+
+          $( ".card-layout" ).tabs();
       }
 
       function addCard(title, col) {
-        const editBtn = $("<button>").attr("class", "edit").html("Edit");
+        const editBtn = $("<button>").attr("class", "edit float-right").html("Edit");
         const card = $("<li>").attr("class", "ui-state-default card").html(title).append(editBtn);
         $(document).find("ul." + col.toLowerCase()).append(card);
       }
